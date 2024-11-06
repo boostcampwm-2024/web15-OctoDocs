0d000136700669ff542da98b2a519242c796ef61
yewonJin <jyw966@naver.com>
2024-11-06 10:52:23 +0900

    feat: 에디터 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,24 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+import Sidebar from "./components/sidebar";
+import HoverTrigger from "./components/HoverTrigger";
+import Editor from "./components/editor";
+import { defaultEditorContent } from "./components/editor/content";
+import SideWrapper from "./components/layout/SideWrapper";
+
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="text-4xl">hiiii</div>{" "}
+      <div className="h-screen bg-[#231F20]">
+        <SideWrapper side="right">
+          <Editor initialValue={defaultEditorContent} />
+        </SideWrapper>
+        <HoverTrigger className="w-64">
+          <Sidebar />
+        </HoverTrigger>
+      </div>
     </QueryClientProvider>
   );
 }
