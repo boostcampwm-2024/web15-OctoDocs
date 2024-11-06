75006cbcaebbf71bda3cf4a505a36dc639c53c75
yewonJin <jyw966@naver.com>
2024-11-06 10:52:23 +0900

    feat: 에디터 구현

---
+++ frontend/src/components/editor/extensions.ts
@@ -0,0 +1,141 @@
+import {
+  TiptapImage,
+  TiptapLink,
+  UpdatedImage,
+  TaskList,
+  TaskItem,
+  HorizontalRule,
+  StarterKit,
+  Placeholder,
+  AIHighlight,
+  CodeBlockLowlight,
+  Mathematics,
+} from "novel/extensions";
+import { UploadImagesPlugin } from "novel/plugins";
+import { cx } from "class-variance-authority";
+import { all, createLowlight } from "lowlight";
+
+const aiHighlight = AIHighlight;
+const placeholder = Placeholder;
+const tiptapLink = TiptapLink.configure({
+  HTMLAttributes: {
+    class: cx(
+      "text-muted-foreground underline underline-offset-[3px] hover:text-primary transition-colors cursor-pointer",
+    ),
+  },
+});
+const lowlight = createLowlight(all);
+
+const tiptapImage = TiptapImage.extend({
+  addProseMirrorPlugins() {
+    return [
+      UploadImagesPlugin({
+        imageClass: cx("opacity-40 rounded-lg border border-stone-200"),
+      }),
+    ];
+  },
+}).configure({
+  allowBase64: true,
+  HTMLAttributes: {
+    class: cx("rounded-lg border border-muted"),
+  },
+});
+
+const updatedImage = UpdatedImage.configure({
+  HTMLAttributes: {
+    class: cx("rounded-lg border border-muted"),
+  },
+});
+
+const taskList = TaskList.configure({
+  HTMLAttributes: {
+    class: cx("not-prose pl-2 "),
+  },
+});
+const taskItem = TaskItem.configure({
+  HTMLAttributes: {
+    class: cx("flex gap-2 items-start my-4"),
+  },
+  nested: true,
+});
+
+const horizontalRule = HorizontalRule.configure({
+  HTMLAttributes: {
+    class: cx("mt-4 mb-6 border-t border-muted-foreground"),
+  },
+});
+
+const starterKit = StarterKit.configure({
+  bulletList: {
+    HTMLAttributes: {
+      class: cx("list-disc list-outside leading-3 -mt-2"),
+    },
+  },
+  orderedList: {
+    HTMLAttributes: {
+      class: cx("list-decimal list-outside leading-3 -mt-2"),
+    },
+  },
+  listItem: {
+    HTMLAttributes: {
+      class: cx("leading-normal -mb-2"),
+    },
+  },
+  blockquote: {
+    HTMLAttributes: {
+      class: cx("border-l-4 border-primary"),
+    },
+  },
+  codeBlock: {
+    HTMLAttributes: {
+      class: cx(
+        "rounded-md bg-muted text-muted-foreground border p-5 font-mono font-medium bg-slate-200",
+      ),
+    },
+  },
+  code: {
+    HTMLAttributes: {
+      class: cx("rounded-md bg-muted px-1.5 py-1 font-mono font-medium"),
+      spellcheck: "false",
+    },
+  },
+  paragraph: {
+    HTMLAttributes: {
+      class: cx(""),
+    },
+  },
+  horizontalRule: false,
+  dropcursor: {
+    color: "#DBEAFE",
+    width: 4,
+  },
+  gapcursor: false,
+});
+
+const codeBlockLowlight = CodeBlockLowlight.configure({
+  // configure lowlight: common /  all / use highlightJS in case there is a need to specify certain language grammars only
+  // common: covers 37 language grammars which should be good enough in most cases
+  lowlight,
+});
+
+const mathematics = Mathematics.configure({
+  HTMLAttributes: {
+    class: cx("text-foreground rounded p-1 hover:bg-accent cursor-pointer"),
+  },
+  katexOptions: {
+    throwOnError: false,
+  },
+});
+export const defaultExtensions = [
+  starterKit,
+  placeholder,
+  tiptapLink,
+  tiptapImage,
+  updatedImage,
+  taskList,
+  taskItem,
+  horizontalRule,
+  aiHighlight,
+  codeBlockLowlight,
+  mathematics,
+];
---
+++ frontend/src/components/layout/SideWrapper.tsx
@@ -0,0 +1,17 @@
+const sideStyle: { readonly [key in Side]: string } = {
+  left: "left-0",
+  right: "right-0 ",
+  top: "top-0 left-1/2 -translate-x-1/2",
+  bottom: "bottom-0 left-1/2 -translate-x-1/2",
+};
+
+type Side = "left" | "right" | "top" | "bottom";
+
+type SideWrapperProps = {
+  side: Side;
+  children: React.ReactNode;
+};
+
+export default function SideWrapper({ side, children }: SideWrapperProps) {
+  return <div className={`absolute z-10 ${sideStyle[side]} `}>{children}</div>;
+}
