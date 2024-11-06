438c6e885a7b63a02c163319e10f525e70705af3
summerM <summermin@snu.ac.kr>
2024-11-06 14:04:05 +0900

    feat: 노드 삭제 api

--- backend/src/node/node.controller.ts
+++ backend/src/node/node.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Param } from '@nestjs/common';
 import { NodeService } from './node.service';
 
 @Controller('node')
 export class NodeController {
   constructor(private readonly nodeService: NodeService) {}
+
+  @Delete(':id')
+  deleteNode(@Param('id') id: number): Promise<void> {
+    return this.nodeService.deleteNode(id);
+  }
 }
--- backend/src/node/node.service.ts
+++ backend/src/node/node.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { NodeRepository } from './node.repository';
 
 @Injectable()
-export class NodeService {}
+export class NodeService {
+  constructor(private readonly nodeRepository: NodeRepository) {}
+
+  async deleteNode(id: number): Promise<void> {
+    const node = await this.nodeRepository.findOneBy({ id });
+    if (!node) {
+      throw new NotFoundException(`Node with ID ${id} not found`);
+    }
+    await this.nodeRepository.delete(id);
+  }
+}
