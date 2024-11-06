54187ba99c2192f9ac401b56b50fe232e40fec72
djk01281 <djk01281@gmail.com>
2024-11-06 23:36:40 +0900

    design: 페이지 목록에서 hover시 배경 색상 변경

--- frontend/src/components/sidebar/NoteList.tsx
+++ frontend/src/components/sidebar/NoteList.tsx
@@ -12,9 +12,9 @@ interface NoteListProps {
 
 export default function NoteList({ className }: NoteListProps) {
   return (
-    <div className={cn("flex flex-col gap-1.5 text-sm font-medium", className)}>
+    <div className={cn("flex flex-col gap-0.5 text-sm font-medium", className)}>
       {noteList.map(({ icon, title }) => (
-        <div className="flex flex-row gap-1.5">
+        <div className="flex flex-row gap-1 rounded-sm px-2 py-1 hover:bg-neutral-100">
           <div>{icon}</div>
           <div>{title}</div>
         </div>
--- frontend/src/components/sidebar/index.tsx
+++ frontend/src/components/sidebar/index.tsx
@@ -5,7 +5,7 @@ export default function Sidebar() {
   return (
     <div className="flex w-64 flex-col gap-4 rounded-lg bg-white p-4 text-black">
       <TopNav />
-      <NoteList className="mx-2" />
+      <NoteList className="mx-1" />
     </div>
   );
 }
