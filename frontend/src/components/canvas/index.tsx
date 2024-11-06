3fcb816c1b44d26b93ae0bbd16c7bcb48af4cf13
djk01281 <djk01281@gmail.com>
2024-11-07 00:39:34 +0900

    feat: 기본 Canvas 컴포넌트 추가

---
+++ frontend/src/components/canvas/index.tsx
@@ -0,0 +1,57 @@
+import { cn } from "@/lib/utils";
+
+import { useCallback } from "react";
+import {
+  ReactFlow,
+  MiniMap,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  BackgroundVariant,
+  type OnConnect,
+  type Node,
+  type Edge,
+} from "@xyflow/react";
+
+import "@xyflow/react/dist/style.css";
+
+const initialNodes: Node[] = [
+  { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
+  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
+];
+const initialEdges: Edge[] = [{ id: "e1-2", source: "1", target: "2" }];
+
+const proOptions = { hideAttribution: true };
+
+interface CanvasProps {
+  className?: string;
+}
+
+export default function Canvas({ className }: CanvasProps) {
+  const [nodes, , onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect: OnConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges],
+  );
+
+  return (
+    <div className={cn("", className)}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        proOptions={proOptions}
+      >
+        <Controls />
+        <MiniMap />
+        <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
+      </ReactFlow>
+    </div>
+  );
+}
