d7e2d8339fd94a8f20bcff54a2b302e2c33880cc
Hyunjun KIM <hihj070914@icloud.com>
2024-11-06 22:07:19 +0900

    test: 페이지 목록 반환 테스트 코드 작성

--- backend/src/page/page.service.spec.ts
+++ backend/src/page/page.service.spec.ts
@@ -1,18 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PageService } from './page.service';
+import { PageRepository } from './page.repository';
 
 describe('PageService', () => {
   let service: PageService;
+  let repository: PageRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PageService],
+      providers: [
+        PageService,
+        {
+          provide: PageRepository,
+          useValue: {
+            findAll: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<PageService>(PageService);
+    repository = module.get<PageRepository>(PageRepository);
   });
 
-  it('should be defined', () => {
+  it('서비스 클래스가 정상적으로 인스턴스화된다.', () => {
     expect(service).toBeDefined();
+  });
+
+  it('모든 페이지 목록을 조회할 수 있다.', async () => {
+    jest.spyOn(repository, 'findAll').mockResolvedValue([]);
+    const result = await service.getPages();
+    expect(result).toEqual([]);
   });
 });
