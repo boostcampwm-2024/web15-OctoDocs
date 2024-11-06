7b8159ff59606267abe2c40faae22b92a0de0b53
djk01281 <djk01281@gmail.com>
2024-11-06 22:58:02 +0900

    feat: NoteList 컴포넌트에 icon 렌더링 로직 추가

--- frontend/src/components/sidebar/NoteList.tsx
+++ frontend/src/components/sidebar/NoteList.tsx
@@ -1,6 +1,10 @@
 import { cn } from "@/lib/utils";
 
-const noteTitles = ["🌳 그라운드 룰", "🚩 커밋 컨벤션", "🗂️ 데일리 스크럼 "];
+const noteList = [
+  { icon: "🌳", title: "그라운드 룰" },
+  { icon: "🚩", title: "커밋 컨벤션" },
+  { icon: "🗂️", title: "데일리 스크럼" },
+];
 
 interface NoteListProps {
   className?: string;
@@ -9,8 +13,11 @@ interface NoteListProps {
 export default function NoteList({ className }: NoteListProps) {
   return (
     <div className={cn("flex flex-col gap-1.5 text-sm font-medium", className)}>
-      {noteTitles.map((title) => (
-        <div>{title}</div>
+      {noteList.map(({ icon, title }) => (
+        <div className="flex flex-row gap-1.5">
+          <div>{icon}</div>
+          <div>{title}</div>
+        </div>
       ))}
     </div>
   );
