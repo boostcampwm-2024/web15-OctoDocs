50e6360a93f4e4dcbfe90539e77ee359c93670c2
summerM <summermin@snu.ac.kr>
2024-11-06 14:11:00 +0900

    feat: 페이지 삭제 api

--- backend/src/page/page.controller.ts
+++ backend/src/page/page.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Param } from '@nestjs/common';
 import { PageService } from './page.service';
 
 @Controller('page')
 export class PageController {
   constructor(private readonly pageService: PageService) {}
+
+  @Delete(':id')
+  deletePage(@Param('id') id: number): Promise<void> {
+    return this.pageService.deletePage(id);
+  }
 }
--- backend/src/page/page.service.ts
+++ backend/src/page/page.service.ts
@@ -1,7 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PageRepository } from './page.repository';
 
 @Injectable()
 export class PageService {
   constructor(private pageRepository: PageRepository) {}
+
+  async deletePage(id: number): Promise<void> {
+    const page = await this.pageRepository.findOneBy({ id });
+    if (!page) {
+      throw new NotFoundException(`Page with ID ${id} not found`);
+    }
+    await this.pageRepository.delete(id);
+  }
 }
