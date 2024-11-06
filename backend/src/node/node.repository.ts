96ac98d7d625d3a82947008f3cf29d8f59080dbf
Hyunjun KIM <hihj070914@icloud.com>
2024-11-06 21:47:11 +0900

    feat: 노드 id를 통해 엔티티 조회하는 레포지토리 작성

--- backend/src/node/node.repository.ts
+++ backend/src/node/node.repository.ts
@@ -7,4 +7,8 @@ export class NodeRepository extends Repository<Node> {
   constructor(private dataSource: DataSource) {
     super(Node, dataSource.createEntityManager());
   }
+
+  async findById(id: number): Promise<Node | null> {
+    return await this.findOneBy({ id });
+  }
 }
