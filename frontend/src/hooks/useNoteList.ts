7cbb8ffcf461b19d336a8e56b698b1ca13d21328
yewonJin <jyw966@naver.com>
2024-11-07 21:20:05 +0900

    feat: noteList 커스텀 훅 구현

---
+++ frontend/src/hooks/useNoteList.ts
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+import usePageStore from "@/store/usePageStore";
+import { useDeletePage, usePages } from "./usePages";
+
+export const useNoteList = () => {
+  const { setCurrentPage } = usePageStore();
+  const { data } = usePages();
+
+  const [noteIdToDelete, setNoteIdToDelete] = useState<number | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const deleteMutation = useDeletePage();
+
+  const handleNoteClick = (id: number) => {
+    setCurrentPage(id);
+  };
+
+  const openModal = (noteId: number) => {
+    setNoteIdToDelete(noteId);
+    setIsModalOpen(true);
+  };
+
+  const onConfirm = () => {
+    if (noteIdToDelete === null) {
+      return;
+    }
+
+    deleteMutation.mutate({ id: noteIdToDelete });
+    setIsModalOpen(false);
+  };
+
+  const onCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return {
+    data,
+    isModalOpen,
+    handleNoteClick,
+    openModal,
+    onConfirm,
+    onCloseModal,
+  };
+};
