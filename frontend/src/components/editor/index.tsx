10c78777bda3f2fbf1449b31a0edcedab0515869
djk01281 <djk01281@gmail.com>
2024-11-07 19:55:39 +0900

    feat: 자동 저장 상태 표시 기능 추가

--- frontend/src/components/editor/index.tsx
+++ frontend/src/components/editor/index.tsx
@@ -42,6 +42,7 @@ const Editor = ({ pageId, initialValue }: EditorProp) => {
   const [openNode, setOpenNode] = useState(false);
   const [openColor, setOpenColor] = useState(false);
   const [openLink, setOpenLink] = useState(false);
+  const [saveStatus, setSaveStatus] = useState("Saved");
 
   const updatePageMutation = useUpdatePage();
 
@@ -50,7 +51,13 @@ const Editor = ({ pageId, initialValue }: EditorProp) => {
       if (pageId === undefined) return;
 
       const json = editor.getJSON();
-      updatePageMutation.mutate({ id: pageId, pageData: json });
+      const response = await updatePageMutation.mutateAsync({
+        id: pageId,
+        pageData: json,
+      });
+      if (response) {
+        setSaveStatus("Saved");
+      }
     },
 
     500,
@@ -62,69 +69,77 @@ const Editor = ({ pageId, initialValue }: EditorProp) => {
   }, [pageId]);
 
   return (
-    <EditorRoot>
-      <EditorContent
-        initialContent={initialContent === null ? undefined : initialContent}
-        className="relative h-[720px] w-[520px] overflow-auto border-muted bg-background bg-white sm:rounded-lg sm:border sm:shadow-lg"
-        extensions={extensions}
-        editorProps={{
-          handleDOMEvents: {
-            keydown: (_view, event) => handleCommandNavigation(event),
-          },
-          attributes: {
-            class: `prose prose-lg prose-headings:font-title font-default focus:outline-none max-w-full`,
-          },
-        }}
-        slotAfter={<ImageResizer />}
-        onUpdate={({ editor }) => {
-          debouncedUpdates(editor);
-        }}
-      >
-        <EditorCommand className="z-50 h-auto max-h-[330px] overflow-y-auto rounded-md border border-muted bg-background px-1 py-2 shadow-md transition-all">
-          <EditorCommandEmpty className="px-2 text-muted-foreground">
-            No results
-          </EditorCommandEmpty>
-          <EditorCommandList>
-            {suggestionItems.map((item) => (
-              <EditorCommandItem
-                value={item.title}
-                onCommand={(val) => item.command?.(val)}
-                className="flex w-full items-center space-x-2 rounded-md px-2 py-1 text-left text-sm hover:cursor-pointer hover:bg-accent aria-selected:bg-accent"
-                key={item.title}
-              >
-                <div className="flex h-10 w-10 items-center justify-center rounded-md border border-muted bg-background">
-                  {item.icon}
-                </div>
-                <div>
-                  <p className="font-medium">{item.title}</p>
-                  <p className="text-xs text-muted-foreground">
-                    {item.description}
-                  </p>
-                </div>
-              </EditorCommandItem>
-            ))}
-          </EditorCommandList>
-        </EditorCommand>
-        <EditorBubble
-          tippyOptions={{
-            placement: "top",
+    <div className="relative h-[720px] w-[520px] overflow-auto border-muted bg-background bg-white sm:rounded-lg sm:border sm:shadow-lg">
+      <div className="absolute right-5 top-5 z-10 mb-5 flex gap-2">
+        <div className="rounded-lg bg-accent px-2 py-1 text-sm text-muted-foreground">
+          {saveStatus}
+        </div>
+      </div>
+      <EditorRoot>
+        <EditorContent
+          initialContent={initialContent === null ? undefined : initialContent}
+          className=""
+          extensions={extensions}
+          editorProps={{
+            handleDOMEvents: {
+              keydown: (_view, event) => handleCommandNavigation(event),
+            },
+            attributes: {
+              class: `prose prose-lg prose-headings:font-title font-default focus:outline-none max-w-full`,
+            },
           }}
-          className="flex w-fit max-w-[90vw] overflow-hidden rounded-md border border-muted bg-background shadow-xl"
+          slotAfter={<ImageResizer />}
+          onUpdate={({ editor }) => {
+            debouncedUpdates(editor);
+            setSaveStatus("Unsaved");
+          }}
         >
-          {" "}
-          <Separator orientation="vertical" />
-          <NodeSelector open={openNode} onOpenChange={setOpenNode} />
-          <Separator orientation="vertical" />
-          <LinkSelector open={openLink} onOpenChange={setOpenLink} />
-          <Separator orientation="vertical" />
-          <MathSelector />
-          <Separator orientation="vertical" />
-          <TextButtons />
-          <Separator orientation="vertical" />
-          <ColorSelector open={openColor} onOpenChange={setOpenColor} />
-        </EditorBubble>
-      </EditorContent>
-    </EditorRoot>
+          <EditorCommand className="z-50 h-auto max-h-[330px] overflow-y-auto rounded-md border border-muted bg-background px-1 py-2 shadow-md transition-all">
+            <EditorCommandEmpty className="px-2 text-muted-foreground">
+              No results
+            </EditorCommandEmpty>
+            <EditorCommandList>
+              {suggestionItems.map((item) => (
+                <EditorCommandItem
+                  value={item.title}
+                  onCommand={(val) => item.command?.(val)}
+                  className="flex w-full items-center space-x-2 rounded-md px-2 py-1 text-left text-sm hover:cursor-pointer hover:bg-accent aria-selected:bg-accent"
+                  key={item.title}
+                >
+                  <div className="flex h-10 w-10 items-center justify-center rounded-md border border-muted bg-background">
+                    {item.icon}
+                  </div>
+                  <div>
+                    <p className="font-medium">{item.title}</p>
+                    <p className="text-xs text-muted-foreground">
+                      {item.description}
+                    </p>
+                  </div>
+                </EditorCommandItem>
+              ))}
+            </EditorCommandList>
+          </EditorCommand>
+          <EditorBubble
+            tippyOptions={{
+              placement: "top",
+            }}
+            className="flex w-fit max-w-[90vw] overflow-hidden rounded-md border border-muted bg-background shadow-xl"
+          >
+            {" "}
+            <Separator orientation="vertical" />
+            <NodeSelector open={openNode} onOpenChange={setOpenNode} />
+            <Separator orientation="vertical" />
+            <LinkSelector open={openLink} onOpenChange={setOpenLink} />
+            <Separator orientation="vertical" />
+            <MathSelector />
+            <Separator orientation="vertical" />
+            <TextButtons />
+            <Separator orientation="vertical" />
+            <ColorSelector open={openColor} onOpenChange={setOpenColor} />
+          </EditorBubble>
+        </EditorContent>
+      </EditorRoot>
+    </div>
   );
 };
 
