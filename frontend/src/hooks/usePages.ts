5bb583fb851b4423d50fce348e8f7e39294142a5
djk01281 <djk01281@gmail.com>
2024-11-07 19:37:54 +0900

    fix: 변경된 /pages api에 맞춰서 useUpdatePage hook 수정

--- frontend/src/hooks/usePages.ts
+++ frontend/src/hooks/usePages.ts
@@ -40,13 +40,8 @@ export const useDeletePage = () => {
 };
 
 export const useUpdatePage = () => {
-  const queryClient = useQueryClient();
-
   return useMutation({
     mutationFn: ({ id, pageData }: { id: number; pageData: PageRequest }) =>
       updatePage(id, pageData),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["pages"] });
-    },
   });
 };
