5b04f7b4043cdf3e7491d4fbc8385cc90d3cc2ee
Hyunjun KIM <hihj070914@icloud.com>
2024-11-12 17:34:59 +0900

    feat: 노드 이동 API 엔드포인트 작성

--- backend/src/node/node.controller.ts
+++ backend/src/node/node.controller.ts
@@ -13,12 +13,14 @@ import {
 import { NodeService } from './node.service';
 import { CreateNodeDto } from './dtos/createNode.dto';
 import { UpdateNodeDto } from './dtos/updateNode.dto';
+import { MoveNodeDto } from './dtos/moveNode.dto';
 
 export enum NodeResponseMessage {
   NODE_CREATED = '노드와 페이지를 생성했습니다.',
   NODE_UPDATED = '노드와 페이지를 갱신했습니다.',
   NODE_DELETED = '노드와 페이지를 삭제했습니다.',
   NODE_GET_COORDINAE = '노드의 현재 좌표를 가져왔습니다.',
+  NODE_MOVED = '노드의 위치를 이동했습니다.',
 }
 
 @Controller('node')
@@ -27,7 +29,7 @@ export class NodeController {
 
   @Post('/')
   @HttpCode(HttpStatus.CREATED)
-  async createNode(@Body() body: CreateNodeDto): Promise<{ message: string }> {
+  async createNode(@Body() body: CreateNodeDto) {
     await this.nodeService.createNode(body);
     return {
       message: NodeResponseMessage.NODE_CREATED,
@@ -62,6 +64,15 @@ export class NodeController {
     return {
       message: NodeResponseMessage.NODE_GET_COORDINAE,
       coordinate: coordinate,
+    };
+  }
+
+  @Patch('/:id/move')
+  @HttpCode(HttpStatus.OK)
+  async moveNode(@Param('id') id: number, @Body() body: MoveNodeDto) {
+    await this.nodeService.moveNode(id, body);
+    return {
+      message: NodeResponseMessage.NODE_MOVED,
     };
   }
 }
