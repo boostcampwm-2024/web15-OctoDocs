dbb6ac733d27e636f736681669b61e15045160f1
djk01281 <djk01281@gmail.com>
2024-11-13 11:54:26 +0900

    fix: 테스트용 enableCors() 제거

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new WsAdapter(app));
   app.useGlobalFilters(new HttpExceptionFilter());
-  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('OctoDocs')
