6121b257e1ad4d7cef974d99ef479b41e1563a63
summerM <summermin@snu.ac.kr>
2024-11-13 20:14:16 +0900

    fix: 의존성 문제 해결

--- backend/src/edge/edge.module.ts
+++ backend/src/edge/edge.module.ts
@@ -1,12 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { EdgeService } from './edge.service';
 import { EdgeController } from './edge.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Edge } from './edge.entity';
 import { EdgeRepository } from './edge.repository';
+import { NodeModule } from 'src/node/node.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Edge])],
+  imports: [TypeOrmModule.forFeature([Edge]), forwardRef(() => NodeModule)],
   controllers: [EdgeController],
   providers: [EdgeService, EdgeRepository],
 })
