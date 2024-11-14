5fe029f03350fcbc728d4ce9359566be8ac96ba1
ez <105545215+ezcolin2@users.noreply.github.com>
2024-11-14 02:30:35 +0000

    feat: dotenv에서 CORS 허용할 origin 등록

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,9 +3,12 @@ import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './filter/http-exception.filter';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { WsAdapter } from '@nestjs/platform-ws';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
   app.useWebSocketAdapter(new WsAdapter(app));
   app.useGlobalFilters(new HttpExceptionFilter());
   
@@ -13,9 +16,12 @@ async function bootstrap() {
     .setTitle('OctoDocs')
     .setDescription('OctoDocs API 명세서')
     .build();
+  console.log(process.env.origin);
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
-
+  app.enableCors({
+    origin: process.env.origin
+  });
   await app.listen(3000);
 }
 bootstrap();
