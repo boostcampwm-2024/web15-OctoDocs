18665aff393d2a3b5bba64d203b18bbc2e06925a
djk01281 <djk01281@gmail.com>
2024-11-14 21:09:09 +0900

    fix: 사이드바에 ScrollWrapper 적용

--- frontend/src/components/layout/ScrollWrapper.tsx
+++ frontend/src/components/layout/ScrollWrapper.tsx
@@ -5,12 +5,18 @@ type ScrollWrapperProps = {
   width?: Tailwindest["width"];
   height?: Tailwindest["height"];
   children: React.ReactNode;
+  className?: string;
 };
 
 export default function ScrollWrapper({
   width,
   height,
   children,
+  className,
 }: ScrollWrapperProps) {
-  return <div className={cn("overflow-auto", width, height)}>{children}</div>;
+  return (
+    <div className={cn("overflow-auto", width, height, className)}>
+      {children}
+    </div>
+  );
 }
