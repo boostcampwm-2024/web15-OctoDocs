e34bf6bda70dc85f33531bbc12a163c2a1cebb05
Hyunjun KIM <hihj070914@icloud.com>
2024-11-19 11:42:13 +0900

    feat: 이미지 업로드 응답 DTO 분리

--- apps/backend/src/upload/upload.controller.ts
+++ apps/backend/src/upload/upload.controller.ts
@@ -7,6 +7,11 @@ import {
 import { FileInterceptor } from '@nestjs/platform-express';
 import { UploadService } from './upload.service';
 import { uploadOptions } from './upload.config';
+import { ImageUploadResponseDto } from './dtos/imageUploadResponse.dto';
+
+export enum UploadResponseMessage {
+  UPLOAD_IMAGE_SUCCESS = '이미지 업로드 성공',
+}
 
 @Controller('upload')
 export class UploadController {
@@ -14,8 +19,13 @@ export class UploadController {
 
   @Post('image')
   @UseInterceptors(FileInterceptor('file', uploadOptions))
-  async uploadImage(@UploadedFile() file: Express.Multer.File) {
+  async uploadImage(
+    @UploadedFile() file: Express.Multer.File,
+  ): Promise<ImageUploadResponseDto> {
     const result = await this.uploadService.uploadImageToCloud(file);
-    return result;
+    return {
+      message: UploadResponseMessage.UPLOAD_IMAGE_SUCCESS,
+      url: result,
+    };
   }
 }
