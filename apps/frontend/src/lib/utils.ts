c2416ad072541728b3c9cb2e3411dcf189506b87
yewonJin <jyw966@naver.com>
2024-11-19 20:05:02 +0900

    feat: 접속 인원 체크를 위한 user store 생성

--- apps/frontend/src/lib/utils.ts
+++ apps/frontend/src/lib/utils.ts
@@ -4,3 +4,23 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function getRandomColor() {
+  const COLORS = [
+    "#7d7b94",
+    "#41c76d",
+    "#f86e7e",
+    "#f6b8b8",
+    "#f7d353",
+    "#3b5bf7",
+    "#59cbf7",
+  ] as const;
+
+  return COLORS[Math.floor(Math.random() * COLORS.length)];
+}
+
+export function getRandomHexString(length = 10) {
+  return [...Array(length)]
+    .map(() => Math.floor(Math.random() * 16).toString(16))
+    .join("");
+}
