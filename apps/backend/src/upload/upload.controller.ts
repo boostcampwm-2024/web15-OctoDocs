aac3e8e0eda169031e0e59b9ec87ed7ee00d5a95
Hyunjun KIM <hihj070914@icloud.com>
2024-11-19 11:37:58 +0900

    feat: 파일 인터셉터 옵션 분리

--- apps/backend/src/upload/upload.controller.ts
+++ apps/backend/src/upload/upload.controller.ts
@@ -6,26 +6,14 @@ import {
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { UploadService } from './upload.service';
-import { InvalidFileException } from 'src/exception/upload.exception';
+import { uploadOptions } from './upload.config';
 
 @Controller('upload')
 export class UploadController {
   constructor(private readonly uploadService: UploadService) {}
 
   @Post('image')
-  @UseInterceptors(
-    FileInterceptor('file', {
-      fileFilter: (req, file, callback) => {
-        if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
-          return callback(new InvalidFileException(), false);
-        }
-        callback(null, true);
-      },
-      limits: {
-        fileSize: 1024 * 1024 * 5, // 5MB
-      },
-    }),
-  )
+  @UseInterceptors(FileInterceptor('file', uploadOptions))
   async uploadImage(@UploadedFile() file: Express.Multer.File) {
     const result = await this.uploadService.uploadImageToCloud(file);
     return result;
