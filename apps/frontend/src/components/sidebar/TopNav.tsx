3a2d78c0f010fc129a0dce20aa099cf4323c31a4
djk01281 <djk01281@gmail.com>
2024-11-21 00:17:01 +0900

    feat: 사이드 바, 에디터 수정

--- apps/frontend/src/components/sidebar/TopNav.tsx
+++ apps/frontend/src/components/sidebar/TopNav.tsx
@@ -2,30 +2,44 @@ import VerticalDivider from "@/components/commons/divider/VerticalDivider";
 import WorkspaceNav from "@/components/WorkspaceNav";
 import LogoBtn from "@/components/LogoBtn";
 import ProfileModal from "./ProfileModal";
-import workspaceLogo from "@/../public/workspace-logo.svg?url";
 import { useState } from "react";
+import { Menu } from "lucide-react";
+import { X } from "lucide-react";
 
-export default function TopNav() {
+interface TopNavProps {
+  onExpand: () => void;
+  isExpanded: boolean;
+}
+export default function TopNav({ onExpand, isExpanded }: TopNavProps) {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
-    <div className="flex items-center gap-2">
-      <LogoBtn
-        onClick={() => {
-          setIsModalOpen(true);
-        }}
-      />
-      <ProfileModal
-        isOpen={isModalOpen}
-        onCloseModal={() => {
-          setIsModalOpen(false);
-        }}
-        onConfirm={() => {
-          setIsModalOpen(false);
-        }}
-      />
-      <VerticalDivider className="h-3" />
-      <WorkspaceNav imageUrl={workspaceLogo} workspaceTitle="프로젝트 Web15" />
+    <div className="flex w-full flex-row items-center justify-between">
+      <div className="flex flex-row items-center gap-2">
+        <LogoBtn
+          onClick={() => {
+            setIsModalOpen(true);
+          }}
+        />
+        <ProfileModal
+          isOpen={isModalOpen}
+          onCloseModal={() => {
+            setIsModalOpen(false);
+          }}
+          onConfirm={() => {
+            setIsModalOpen(false);
+          }}
+        />
+        <VerticalDivider className="h-3" />
+        <WorkspaceNav workspaceTitle="프로젝트 Web15" />
+      </div>
+      <button onClick={onExpand}>
+        {isExpanded ? (
+          <X color="#3F3F3F" />
+        ) : (
+          <Menu size={24} color="#3F3F3F" />
+        )}
+      </button>
     </div>
   );
 }
--- apps/frontend/src/components/sidebar/index.tsx
+++ apps/frontend/src/components/sidebar/index.tsx
@@ -1,13 +1,29 @@
 import NoteList from "./NoteList";
 import TopNav from "./TopNav";
 import Tools from "./Tools";
+import { useState } from "react";
+import ScrollWrapper from "@/components/layout/ScrollWrapper";
 
 export default function Sidebar() {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
-    <div className="z-10 flex w-64 flex-col gap-4 rounded-lg border-[1px] border-neutral-200 bg-white p-4 text-black shadow-sm">
-      <TopNav />
-      <Tools />
-      <NoteList className="mx-1" />
+    <div className="z-10 flex w-64 flex-col rounded-xl border-[1px] border-neutral-200 bg-white text-black shadow-md">
+      <div className="p-4">
+        <TopNav onExpand={handleExpand} isExpanded={isExpanded} />
+      </div>
+      <div className={`${isExpanded ? "flex flex-col" : "hidden"} gap-3 pb-4`}>
+        <div className="w-full px-4">
+          <Tools />
+        </div>
+        <ScrollWrapper className="scrollbar scrollbar-thumb-[#d9d9d9] scrollbar-track-transparent max-h-[612px] overflow-x-clip">
+          <NoteList className="p-4 pb-0 pt-0" />
+        </ScrollWrapper>
+      </div>
     </div>
   );
 }
