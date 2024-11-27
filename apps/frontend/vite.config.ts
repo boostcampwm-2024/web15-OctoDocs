8b6c5f88d7b38ef27f281a4260e801365ba35669
Tolerblanc <hihj070914@icloud.com>
2024-11-27 16:30:52 +0900

    feat: windows에서도 HMR이 동작하도록 폴링옵션 추가

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -13,6 +13,10 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 5173,
+    watch: {
+      usePolling: true,
+      interval: 1000,
+    },
     hmr: {
       protocol: "wss",
       clientPort: 443,
