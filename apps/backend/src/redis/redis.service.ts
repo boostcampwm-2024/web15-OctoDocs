6c4c5b6c403aa5633c830da55134f0f49e22702c
ez <105545215+ezcolin2@users.noreply.github.com>
2024-11-27 15:45:20 +0900

    refactor: title, content 변경 사항 redis에 저장 구현

--- apps/backend/src/redis/redis.service.ts
+++ apps/backend/src/redis/redis.service.ts
@@ -9,15 +9,18 @@ export class RedisService {
     this.redisClient = new Redis({
       host: process.env.REDIS_HOST,
       port: parseInt(process.env.REDIS_PORT),
-      password: process.env.REDIS_PASSWORD,
     });
   }
 
   async get(key: string) {
-    return await this.redisClient.hgetall(key);
+    const data = await this.redisClient.hgetall(key);
+    return Object.fromEntries(
+      Object.entries(data).map(([field, value]) => [field, value]),
+    );
   }
 
   async set(key: string, value: object) {
+    console.log('set', Object.entries(value));
     return await this.redisClient.hset(key, Object.entries(value));
   }
 
--- apps/backend/src/yjs/yjs.module.ts
+++ apps/backend/src/yjs/yjs.module.ts
@@ -3,9 +3,11 @@ import { YjsService } from './yjs.service';
 import { NodeModule } from 'src/node/node.module';
 import { PageModule } from '../page/page.module';
 import { EdgeModule } from '../edge/edge.module';
+import { RedisModule } from '../redis/redis.module';
+import { RedisService } from '../redis/redis.service';
 
 @Module({
-  imports: [NodeModule, PageModule, EdgeModule],
-  providers: [YjsService],
+  imports: [NodeModule, PageModule, EdgeModule, RedisModule],
+  providers: [YjsService, RedisService],
 })
 export class YjsModule {}
