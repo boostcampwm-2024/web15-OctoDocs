f68014cc80a6144b46679a87c7d9228691989810
Hyunjun KIM <hihj070914@icloud.com>
2024-11-27 13:20:35 +0900

    feat: vite 빌드 설정 및 커맨드 변경

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -10,4 +10,13 @@ export default defineConfig({
       plugins: [tailwindcss()],
     },
   },
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    hmr: {
+      protocol: "wss",
+      clientPort: 443,
+      path: "hmr/",
+    },
+  },
 });
