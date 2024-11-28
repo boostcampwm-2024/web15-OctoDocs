a7a1edf2df5ec36c3edec772c1262a155a2e1146
yewonJin <jyw966@naver.com>
2024-11-28 20:58:05 +0900

    feat: 현재 참여하고 있는 워크스페이스 이름 표기

--- apps/frontend/src/widgets/TopNavView/ui/index.tsx
+++ apps/frontend/src/widgets/TopNavView/ui/index.tsx
@@ -1,20 +1,34 @@
+import { useParams } from "@tanstack/react-router";
 import { Menu, X } from "lucide-react";
 
 import { Divider } from "@/shared/ui";
 import { WorkspaceNav } from "@/features/pageSidebar/ui";
 import { UserInfoView } from "@/widgets/UserInfoView/ui";
+import { useUserWorkspace } from "@/features/workspace/model/useWorkspace";
 
 interface TopNavProps {
   onExpand: () => void;
   isExpanded: boolean;
 }
 export function TopNavView({ onExpand, isExpanded }: TopNavProps) {
+  const { workspaceId } = useParams({ strict: false });
+  const { data } = useUserWorkspace();
+
+  const getWorkspaceTitle = () => {
+    if (!workspaceId) return "공용 워크스페이스";
+
+    return (
+      data?.workspaces.find((w) => w.workspaceId === workspaceId)?.title ??
+      "로딩 중..."
+    );
+  };
+
   return (
     <div className="flex w-full flex-row items-center justify-between">
       <div className="flex flex-row items-center gap-2">
         <UserInfoView />
         <Divider direction="vertical" className="h-3" />
-        <WorkspaceNav workspaceTitle="프로젝트 Web15" />
+        <WorkspaceNav workspaceTitle={getWorkspaceTitle()} />
       </div>
       <div className="flex h-7 w-7 items-center justify-center">
         <button onClick={onExpand}>
