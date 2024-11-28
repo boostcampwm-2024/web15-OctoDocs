283b5ddaad4391839aaf4327262c8d1457ea951f
yewonJin <jyw966@naver.com>
2024-11-28 13:54:39 +0900

    feat: 워크스페이스 query 추가

---
+++ apps/frontend/src/features/workspace/model/useWorkspace.ts
@@ -0,0 +1,38 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
+import {
+  createWorkspace,
+  getUserWorkspaces,
+  removeWorkspace,
+} from "../api/workspaceApi";
+
+export const useUserWorkspace = (userId: string) => {
+  return useQuery({
+    queryKey: ["userWorkspace", userId],
+    queryFn: () => getUserWorkspaces(userId),
+  });
+};
+
+// response로 workspaceId가 오는데 userWorkspace를 어떻게 invalidate 할까?
+// login state에 있는 userId로?
+export const useCreateWorkspace = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: createWorkspace,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["userWorkspace"] });
+    },
+  });
+};
+
+export const useRemoveWorkspace = (w) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: removeWorkspace,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["userWorkspace"] });
+    },
+  });
+};
