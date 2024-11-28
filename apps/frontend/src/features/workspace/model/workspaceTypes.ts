7de1b06710a68a0b8073abe8516f7c64501d731d
yewonJin <jyw966@naver.com>
2024-11-28 13:39:35 +0900

    feat: 워크스페이스 query API Types 추가

---
+++ apps/frontend/src/features/workspace/model/workspaceTypes.ts
@@ -0,0 +1,28 @@
+export interface Workspace {
+  workspaceId: string;
+  title: string;
+  description: string;
+  thumbnailUrl: string;
+  role: "owner" | "guest";
+}
+
+export interface CreateWorkSpaceResquest {
+  title: string;
+  description: string;
+  visibility: "private" | "public";
+  thumbnailUrl: string;
+}
+
+export interface CreateWorkSpaceResponse {
+  message: string;
+  workspaceId: string;
+}
+
+export interface RemoveWorkSpaceResponse {
+  message: string;
+}
+
+export interface GetUserWorkspaceResponse {
+  message: string;
+  workspaces: Workspace[];
+}
