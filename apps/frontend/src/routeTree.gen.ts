d8757931fafa52b43aba8dc36c316331e9e68d57
djk01281 <djk01281@gmail.com>
2024-11-28 18:07:19 +0900

    fix: routeTree 파일 수정

--- apps/frontend/src/routeTree.gen.ts
+++ apps/frontend/src/routeTree.gen.ts
@@ -10,104 +10,104 @@
 
 // Import Routes
 
-import { Route as rootRoute } from './routes/__root'
-import { Route as IndexImport } from './routes/index'
-import { Route as JoinIndexImport } from './routes/join/index'
-import { Route as WorkspaceWorkspaceIdImpo./routes/joinworkspace/$workspaceId'
+import { Route as rootRoute } from "./routes/__root";
+import { Route as IndexImport } from "./routes/index";
+import { Route as JoinIndexImport } from "./routes/join/index";
+import { Route as WorkspaceWorkspaceIdImport } from "./routes/joinworkspace/$workspaceId";
 
 // Create/Update Routes
 
 const IndexRoute = IndexImport.update({
-  id: '/',
-  path: '/',
+  id: "/",
+  path: "/",
   getParentRoute: () => rootRoute,
-} as any)
+} as any);
 
 const JoinIndexRoute = JoinIndexImport.update({
-  id: '/join/',
-  path: '/join/',
+  id: "/join/",
+  path: "/join/",
   getParentRoute: () => rootRoute,
-} as any)
+} as any);
 
 const WorkspaceWorkspaceIdRoute = WorkspaceWorkspaceIdImport.update({
-  id: '/workspace/$workspaceId',
-  path: '/workspace/$workspaceId',
+  id: "/workspace/$workspaceId",
+  path: "/workspace/$workspaceId",
   getParentRoute: () => rootRoute,
-} as any)
+} as any);
 
 // Populate the FileRoutesByPath interface
 
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
   interface FileRoutesByPath {
-    '/': {
-      id: '/'
-      path: '/'
-      fullPath: '/'
-      preLoaderRoute: typeof IndexImport
-      parentRoute: typeof rootRoute
-    }
-    '/workspace/$workspaceId': {
-      id: '/workspace/$workspaceId'
-      path: '/workspace/$workspaceId'
-      fullPath: '/workspace/$workspaceId'
-      preLoaderRoute: typeof WorkspaceWorkspaceIdImport
-      parentRoute: typeof rootRoute
-    }
-    '/join/': {
-      id: '/join/'
-      path: '/join'
-      fullPath: '/join'
-      preLoaderRoute: typeof JoinIndexImport
-      parentRoute: typeof rootRoute
-    }
+    "/": {
+      id: "/";
+      path: "/";
+      fullPath: "/";
+      preLoaderRoute: typeof IndexImport;
+      parentRoute: typeof rootRoute;
+    };
+    "/workspace/$workspaceId": {
+      id: "/workspace/$workspaceId";
+      path: "/workspace/$workspaceId";
+      fullPath: "/workspace/$workspaceId";
+      preLoaderRoute: typeof WorkspaceWorkspaceIdImport;
+      parentRoute: typeof rootRoute;
+    };
+    "/join/": {
+      id: "/join/";
+      path: "/join";
+      fullPath: "/join";
+      preLoaderRoute: typeof JoinIndexImport;
+      parentRoute: typeof rootRoute;
+    };
   }
 }
 
 // Create and export the route tree
 
 export interface FileRoutesByFullPath {
-  '/': typeof IndexRoute
-  '/workspace/$workspaceId': typeof WorkspaceWorkspaceIdRoute
-  '/join': typeof JoinIndexRoute
+  "/": typeof IndexRoute;
+  "/workspace/$workspaceId": typeof WorkspaceWorkspaceIdRoute;
+  "/join": typeof JoinIndexRoute;
 }
 
 export interface FileRoutesByTo {
-  '/': typeof IndexRoute
-  '/workspace/$workspaceId': typeof WorkspaceWorkspaceIdRoute
-  '/join': typeof JoinIndexRoute
+  "/": typeof IndexRoute;
+  "/workspace/$workspaceId": typeof WorkspaceWorkspaceIdRoute;
+  "/join": typeof JoinIndexRoute;
 }
 
 export interface FileRoutesById {
-  __root__: typeof rootRoute
-  '/': typeof IndexRoute
-  '/workspace/$workspaceId': typeof WorkspaceWorkspaceIdRoute
-  '/join/': typeof JoinIndexRoute
+  __root__: typeof rootRoute;
+  "/": typeof IndexRoute;
+  "/workspace/$workspaceId": typeof WorkspaceWorkspaceIdRoute;
+  "/join/": typeof JoinIndexRoute;
 }
 
 export interface FileRouteTypes {
-  fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/' | '/workspace/$workspaceId' | '/join'
-  fileRoutesByTo: FileRoutesByTo
-  to: '/' | '/workspace/$workspaceId' | '/join'
-  id: '__root__' | '/' | '/workspace/$workspaceId' | '/join/'
-  fileRoutesById: FileRoutesById
+  fileRoutesByFullPath: FileRoutesByFullPath;
+  fullPaths: "/" | "/workspace/$workspaceId" | "/join";
+  fileRoutesByTo: FileRoutesByTo;
+  to: "/" | "/workspace/$workspaceId" | "/join";
+  id: "__root__" | "/" | "/workspace/$workspaceId" | "/join/";
+  fileRoutesById: FileRoutesById;
 }
 
 export interface RootRouteChildren {
-  IndexRoute: typeof IndexRoute
-  WorkspaceWorkspaceIdRoute: typeof WorkspaceWorkspaceIdRoute
-  JoinIndexRoute: typeof JoinIndexRoute
+  IndexRoute: typeof IndexRoute;
+  WorkspaceWorkspaceIdRoute: typeof WorkspaceWorkspaceIdRoute;
+  JoinIndexRoute: typeof JoinIndexRoute;
 }
 
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
   WorkspaceWorkspaceIdRoute: WorkspaceWorkspaceIdRoute,
   JoinIndexRoute: JoinIndexRoute,
-}
+};
 
 export const routeTree = rootRoute
   ._addFileChildren(rootRouteChildren)
-  ._addFileTypes<FileRouteTypes>()
+  ._addFileTypes<FileRouteTypes>();
 
 /* ROUTE_MANIFEST_START
 {
