3d8fd9cc16b6b8ae9400c0ed4e0c469fe64477b1
djk01281 <djk01281@gmail.com>
2024-12-02 21:15:20 +0900

    fix: 워크스페이스 공개 범위 전환 api URL 수정

--- apps/frontend/src/features/workspace/api/workspaceStatusApi.ts
+++ apps/frontend/src/features/workspace/api/workspaceStatusApi.ts
@@ -1,14 +1,12 @@
-import { Post } from "@/shared/api";
+import { Patch } from "@/shared/api";
 import { SetWorkspaceStatusResponse } from "../model/workspaceInviteTypes";
 
 export const setWorkspaceStatusToPrivate = async (id: string) => {
-  // TODO: URL 맞게 고치기.
   const url = `/api/workspace/${id}/private`;
-  await Post<SetWorkspaceStatusResponse, null>(url);
+  await Patch<SetWorkspaceStatusResponse, null>(url);
 };
 
 export const setWorkspaceStatusToPublic = async (id: string) => {
-  // TODO: URL 맞게 고치기.
   const url = `/api/workspace/${id}/public`;
-  await Post<SetWorkspaceStatusResponse, null>(url);
+  await Patch<SetWorkspaceStatusResponse, null>(url);
 };
