daa3a56923d410384b029f859d3c79c9d92baac2
yewonJin <jyw966@naver.com>
2024-12-02 11:05:25 +0900

    chore: Tanstack Query 관련 파일 app 디렉토리로 이동

--- apps/frontend/src/app/main.tsx
+++ apps/frontend/src/app/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import "../shared/index.css";
-import { routeTree } from "@/routeTree.gen";
+import { routeTree } from "@/app/routeTree.gen";
 
 const queryClient = new QueryClient();
 
